93646fd0546ce9427ab8251fa0a5e8bb240793b7
Victor Baesso <jotatyr@gmail.com>
2019-05-22 21:20:43 -0300

    SS

--- RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/controller/UsuarioController.java
+++ RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.bixos.rainBebidas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.bixos.rainBebidas.service.UsuarioService;
 
@@ -10,4 +11,9 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService service;
+
+	@GetMapping("/usuario/novo")
+	public void novo() {
+		// Mapeado para JSP
+	}
 }
