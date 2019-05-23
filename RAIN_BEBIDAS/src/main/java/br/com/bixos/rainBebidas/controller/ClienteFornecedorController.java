6d2cffe289d1a364a6c2b134c7552b1013eeee90
Victor Baesso <jotatyr@gmail.com>
2019-05-23 19:25:08 -0300

    SS

--- RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/controller/ClienteFornecedorController.java
+++ RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/controller/ClienteFornecedorController.java
@@ -1,12 +1,40 @@
 package br.com.bixos.rainBebidas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.bixos.rainBebidas.model.ClienteFornecedor;
 import br.com.bixos.rainBebidas.service.ClienteFornecedorService;
 
 public class ClienteFornecedorController {
 
 	@Autowired
 	private ClienteFornecedorService service;
+
+	@RequestMapping(value = "/cliente", method = RequestMethod.GET)
+	public String visualizar(Model model) {
+		model.addAttribute("clientes", service.listar());
+
+		return "clienteFornecedor/visualizar";
+	}
+
+	@RequestMapping(value = "/cliente/salvar", method = RequestMethod.POST)
+	public ModelAndView salvar(ClienteFornecedor user) {
+		service.salvar(user);
+
+		return new ModelAndView("redirect:/cliente");
+	}
+
+	@GetMapping("/cliente/excluir/{codigo}")
+	public ModelAndView excluir(@PathVariable(value = "codigo") Long codigo) {
+		service.excluir(codigo);
+
+		return new ModelAndView("redirect:/cliente");
+	}
 
 }
--- RAIN_BEBIDAS/src/main/webapp/WEB-INF/views/clienteFornecedor/novo.jsp
+++ RAIN_BEBIDAS/src/main/webapp/WEB-INF/views/clienteFornecedor/novo.jsp
@@ -8,5 +8,28 @@
 </head>
 <body>
 
+	<form action="/usuario/salvar" method="post" class="col">
+							
+		<div class="form-group">
+			<input type="hidden" class="form-control" id="codigo" name="codigo" value="${codigo}">
+			<input type="hidden" class="form-control" id="codigo" name="pessoa.codigo" value="${pessoa.codigo}">
+			<input type="text" class="form-control" id="nome" name="pessoa.nome" placeholder="Nome" value="${pessoa.nome}">
+		</div>
+		<div class="form-group">
+			<input type="text" class="form-control" id="cpf" name="pessoa.cpf" placeholder="CPF" value="${pessoa.cpf}">
+		</div>
+		<div class="form-group">
+			<input type="text" class="form-control" id="cpf" name="cnpj" placeholder="CNPJ" value="${cnpj}">
+		</div>
+		<div class="form-group">
+			<input type="text" class="form-control" id="telefone" name="pessoa.telefone" placeholder="Telefone" value="${pessoa.telefone}">
+		</div>
+		<div class="form-group">
+			<input type="date" class="form-control" id="dtnascimento" name="pessoa.dataNascimento" placeholder="Senha" value="${pessoa.dataNascimento}">
+		</div>
+		
+		<button type="submit" class="btn btn-primary">Salvar</button>
+	
+	</form>
 </body>
 </html>
