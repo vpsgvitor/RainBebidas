7ab2be8ca258848388ab6d8713f77a19c7150128
vpsg_vitor <vpsgvitor@gmail.com>
2019-05-23 21:44:11 -0300

    Se tiver cagada, culpa é do joão

--- RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/Boot.java
+++ RAIN_BEBIDAS/src/main/java/br/com/bixos/rainBebidas/Boot.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot
-{
+public class Boot {
 
-   public static void main(String[] args)
-   {
-      SpringApplication.run(Boot.class, args);
-   }
+	public static void main(String[] args) {
+		SpringApplication.run(Boot.class, args);
+	}
 }
